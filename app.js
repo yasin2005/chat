0ce3d0e96b01d6801bacfa9263fb6694a1a8dccf
yasin2005 <yasin.ibrahim2005@gmail.com>
2023-01-02 14:17:24 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+//dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMesaage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+//add a new chat
+newChatForm.addEventListener('submit', e => {
+ e.preventDefault();
+ const message = newChatForm.message.value.trim();
+ chatroom.addChat(message)
+  .then(() => newChatForm.reset())
+  .catch(err => console.log(err));
+});
+
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    //reset form
+    newNameForm.reset();
+    //show then hide the update message
+    updateMesaage.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMesaage.innerText = ' ', 5000);
+
+});
+//update chatroom
+rooms.addEventListener('click', e => {
+if(e.target.tagName === "BUTTON") {
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+}
+});
+//check local storage for a name
+const username = localStorage.username ? localStorage.username : 'Anonymous';
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+// get chats and render
+chatroom.getChats(data => chatUI.render(data));
---
+++ ui.js
@@ -0,0 +1,25 @@
+// render chat templates to the DOM
+//clear the list of chats (when the room changes)
+
+class ChatUI {
+    constructor(list) {
+     this.list = list; 
+    }
+    clear() {
+      this.list.innerHTML = '';
+    }
+    render(data) {
+        const when = dateFns.distanceInWordsToNow (
+        data.createdAt.toDate(),
+         {addSuffix: true}
+        );
+        const html = `
+        <li class="list-group-item">
+          <span class="username">${data.username}</span>
+          <span class="message">${data.message}</span>
+          <div class="time">${when}</div>
+        </li>
+        `;
+        this.list.innerHTML += html; 
+    }
+}
